fabb5b4419eb2e9281fc5f60f7adc0927bb60d61
Zachary Smith <z.smith32@gmail.com>
2016-10-25 18:47:23 -0700

    finished reading in 1 line at a time, need to integrate static variable to hold state through seperate function calls

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,19 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/17 23:20:10 by zsmith            #+#    #+#             */
-/*   Updated: 2016/10/17 23:47:21 by zsmith           ###   ########.fr       */
+/*   Updated: 2016/10/24 15:56:05 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+/*
 int		get_next_line(const int fd, char **line)
 {
 	static int	index;
 	char		buf[BUFF_SIZE];
-	int			j
+	int			j;
 
 	while (1)
 	{
@@ -38,3 +40,81 @@ int		get_next_line(const int fd, char **line)
 	}	
 
 }
+*/
+
+int		read_to_nl(int fd)
+{
+	char	*buf;
+	char	*ret;
+	int		index;
+	int		counter;
+
+	counter = 0;
+	index = 0;
+	ret = "\0";
+	buf = (char *)malloc(BUFF_SIZE + 1);
+	while ((index = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[index] = '\0';
+		printf("index: %d\n", index);
+		printf("buf: '%s'\n", buf);
+		counter += index;
+		ret = ft_strccat(ret, buf, '\n');
+		printf("ret: %s\n", ret);
+		if (!(check_nl(buf)))
+		{
+			break;
+			ft_bzero(buf, index);
+			buf[index] = '\0';
+		}
+	}
+	printf("out of loop ret: %s\n", ret);
+	return (index);
+}
+
+int		check_nl(char *buf)
+{
+	int		i;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			break;
+		i++;
+	}
+	return (i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,22 @@
 /*   By: zsmith <zsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/17 23:20:16 by zsmith            #+#    #+#             */
-/*   Updated: 2016/10/17 23:41:52 by zsmith           ###   ########.fr       */
+/*   Updated: 2016/10/24 15:38:19 by zsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
 # include <unistd.h>
+# include <stdio.h>
+# include "libft.h"
+
 # define BUFF_SIZE 32
+
+int		check_nl(char *buf);
+int		read_to_nl(int fd);
 
 
 #endif
