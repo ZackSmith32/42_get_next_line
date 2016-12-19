34577f84a38ac3771116e53b4f7f908cd67f0a12
Zack Smith <z.smith32@gmail.com>
2016-12-19 15:28:53 -0500

    still have an unfreed malloc, this is from the buffer on read.  Freeing it would be complicated... tempted to rewrite the whole thing to make it more portable.

--- get_next_line.c
+++ get_next_line.c
@@ -6,97 +6,11 @@
 /*   By: mba <mba@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 16:01:14 by zsmith            #+#    #+#             */
-/*   Updated: 2016/12/19 14:49:52 by mba              ###   ########.fr       */
+/*   Updated: 2016/12/19 15:21:38 by mba              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int		get_next_line(const int fd, char **line)
-{
-	static t_a_list				*holder;
-	t_a_list					*temp;
-	t_a_list					*temp2;
-	int							z;
-
-	if (fd < 0 || line == 0)
-		return (-1);
-	if (holder == NULL)
-		holder = (t_a_list *)ft_memalloc(sizeof(t_a_list));
-	temp = holder;
-	while (temp->fd != fd)
-	{
-		if (temp->next == NULL)
-		{
-			temp2 = (t_a_list *)ft_memalloc(sizeof(t_a_list));
-			temp2->fd = fd;
-			temp->next = temp2;
-			temp = temp->next;
-			break ;
-		}
-		temp = temp->next;
-	}
-	z = central(fd, temp, line);
-	return (z == 0 ? free_struct(&holder) : z);
-}
-
-int		central(int fd, t_a_list *holder, char **line)
-{
-	int		k;
-
-	*line = (char *)ft_memalloc(1);
-	if (!(check_struct(holder, line)))
-		return (1);
-	k = read_buf(fd, holder, line);
-	if (k == 0 && ft_strlen(*line) != 0)
-		k = 1;
-	return (k);
-}
-
-int		check_struct(t_a_list *holder, char **line)
-{
-	int		i;
-	int		index;
-	char	*input;
-
-	i = 0;
-	index = (int)holder->content_size;
-	input = (char*)holder->content;
-	if (!(input))
-		return (1);
-	if (index >= (int)ft_strlen(input))
-		return (1);
-	if (input != 0)
-		holder->content_size += 1;
-	set_line(holder, line);
-	if (holder->content_size != ft_strlen(holder->content))
-		return (0);
-	return (1);
-}
-
-int		read_buf(int fd, t_a_list *holder, char **line)
-{
-	int		i;
-	char	*buf;
-
-	i = 0;
-	buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	while ((i = read(fd, buf, BUFF_SIZE)))
-	{
-		if (i == -1)
-			return (-1);
-		buf[i] = '\0';
-		holder->content = buf;
-		holder->content_size = 0;
-		set_line(holder, line);
-		if (holder->content_size != BUFF_SIZE)
-			break ;
-	}
-	free(buf);
-	if (i == 0)
-		return (0);
-	return (1);
-}
 
 int		set_line(t_a_list *holder, char **line)
 {
@@ -125,4 +39,90 @@ int		set_line(t_a_list *holder, char **line)
 	line[0][(j + i)] = '\0';
 	holder->content_size = holder->content_size + i;
 	return (1);
+}
+
+int		read_buf(int fd, t_a_list *holder, char **line)
+{
+	int		i;
+	char	*buf;
+
+	i = 0;
+	buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	while ((i = read(fd, buf, BUFF_SIZE)))
+	{
+		if (i == -1)
+			return (-1);
+		buf[i] = '\0';
+		holder->content = buf;
+		holder->content_size = 0;
+		set_line(holder, line);
+		if (holder->content_size != BUFF_SIZE)
+			break ;
+	}
+	// free(buf);
+	if (i == 0)
+		return (0);
+	return (1);
+}
+
+int		check_struct(t_a_list *holder, char **line)
+{
+	int		i;
+	int		index;
+	char	*input;
+
+	i = 0;
+	index = (int)holder->content_size;
+	input = (char*)holder->content;
+	if (!(input))
+		return (1);
+	if (index >= (int)ft_strlen(input))
+		return (1);
+	if (input != 0)
+		holder->content_size += 1;
+	set_line(holder, line);
+	if (holder->content_size != ft_strlen(holder->content))
+		return (0);
+	return (1);
+}
+
+int		central(int fd, t_a_list *holder, char **line)
+{
+	int		k;
+
+	*line = (char *)ft_memalloc(1);
+	if (!(check_struct(holder, line)))
+		return (1);
+	k = read_buf(fd, holder, line);
+	if (k == 0 && ft_strlen(*line) != 0)
+		k = 1;
+	return (k);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static t_a_list				*holder;
+	t_a_list					*temp;
+	t_a_list					*temp2;
+	int							z;
+
+	if (fd < 0 || line == 0)
+		return (-1);
+	if (holder == NULL)
+		holder = (t_a_list *)ft_memalloc(sizeof(t_a_list));
+	temp = holder;
+	while (temp->fd != fd)
+	{
+		if (temp->next == NULL)
+		{
+			temp2 = (t_a_list *)ft_memalloc(sizeof(t_a_list));
+			temp2->fd = fd;
+			temp->next = temp2;
+			temp = temp->next;
+			break ;
+		}
+		temp = temp->next;
+	}
+	z = central(fd, temp, line);
+	return (z == 0 ? ft_free_struct(&holder) : z);
 }
